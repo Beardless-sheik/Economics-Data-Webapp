6cf5a22cf056f315eb514de606e6e42f91c4e54b
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-08 23:33:22 +0200

    Create Home.js Page Static Component

---
+++ src/pages/home/home.js
@@ -0,0 +1,55 @@
+import { Fragment } from 'react';
+import './home.css';
+
+const Home = () => (
+  <>
+    <h1 className="mainHeaderText"> VEHICLE CARBON ESTIMATE STATS PER 100KM</h1>
+    <p className="subMainHeaderText">STATS By Models</p>
+    <div className="mainEstimateGrid">
+      <div className="estimateDiv">
+        <div className="imageLogoContainer">
+          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
+        </div>
+        <p className="carModelDescriptionText"> Name of Car Model</p>
+        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+      </div>
+      <div className="estimateDiv">
+        <div className="imageLogoContainer">
+          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
+        </div>
+        <p className="carModelDescriptionText"> Name of Car Model</p>
+        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+      </div>
+      <div className="estimateDiv">
+        <div className="imageLogoContainer">
+          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
+        </div>
+        <p className="carModelDescriptionText"> Name of Car Model</p>
+        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+      </div>
+      <div className="estimateDiv">
+        <div className="imageLogoContainer">
+          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
+        </div>
+        <p className="carModelDescriptionText"> Name of Car Model</p>
+        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+      </div>
+      <div className="estimateDiv">
+        <div className="imageLogoContainer">
+          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
+        </div>
+        <p className="carModelDescriptionText"> Name of Car Model</p>
+        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+      </div>
+      <div className="estimateDiv">
+        <div className="imageLogoContainer">
+          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
+        </div>
+        <p className="carModelDescriptionText"> Name of Car Model</p>
+        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Home;
