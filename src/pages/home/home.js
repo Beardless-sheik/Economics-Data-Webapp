335860f14592d24f5d268ea79a4c041fecc59600
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-09 00:10:29 +0200

    Create ModelArray static data to test components

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,55 +1,41 @@
 import { Fragment } from 'react';
 import './home.css';
+import ModelCarDetails from '../../components/ModelCarDetails/modelCarDetails';
 
-const Home = () => (
-  <>
-    <h1 className="mainHeaderText"> VEHICLE CARBON ESTIMATE STATS PER 100KM</h1>
-    <p className="subMainHeaderText">STATS By Models</p>
-    <div className="mainEstimateGrid">
-      <div className="estimateDiv">
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
-        </div>
-        <p className="carModelDescriptionText"> Name of Car Model</p>
-        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
+const Home = () => {
+  const ModelArrayData = [{
+    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
+    carModelName: 'Alfa Romeo',
+    numberOfModelsAvailable: 567,
+  }, {
+    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
+    carModelName: 'BMW',
+    numberOfModelsAvailable: 1567,
+  }, {
+    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
+    carModelName: 'Benz',
+    numberOfModelsAvailable: 5567,
+  }, {
+    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
+    carModelName: 'Lambo',
+    numberOfModelsAvailable: 55667,
+  },
+  {
+    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
+    carModelName: 'Nissan',
+    numberOfModelsAvailable: 6856,
+  }];
+  return (
+    <>
+      <h1 className="mainHeaderText"> VEHICLE CARBON ESTIMATE STATS PER 100KM</h1>
+      <p className="subMainHeaderText">STATS By Models</p>
+      <div className="mainEstimateGrid">
+        {
+            ModelArrayData.map((element,index) => <ModelCarDetails {...element}/>)
+        }
       </div>
-      <div className="estimateDiv">
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
-        </div>
-        <p className="carModelDescriptionText"> Name of Car Model</p>
-        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
-      </div>
-      <div className="estimateDiv">
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
-        </div>
-        <p className="carModelDescriptionText"> Name of Car Model</p>
-        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
-      </div>
-      <div className="estimateDiv">
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
-        </div>
-        <p className="carModelDescriptionText"> Name of Car Model</p>
-        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
-      </div>
-      <div className="estimateDiv">
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
-        </div>
-        <p className="carModelDescriptionText"> Name of Car Model</p>
-        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
-      </div>
-      <div className="estimateDiv">
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg" />
-        </div>
-        <p className="carModelDescriptionText"> Name of Car Model</p>
-        <p className="carModelDescriptionText sizeOfNumberText"> 567 </p>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
