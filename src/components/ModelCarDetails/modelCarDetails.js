b9043765730f89322ece6f1a589c61c161be6704
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-10 17:30:26 +0200

    Implement routing in modelCarDetail component
    From homepage click on modelCarDetail component takes the user to the detailed page

--- src/components/ModelCarDetails/modelCarDetails.js
+++ src/components/ModelCarDetails/modelCarDetails.js
@@ -1,24 +1,39 @@
 import './modelCarDetails.css';
 import { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 const ModelCarDetails = (props) => {
+  const navigate = useNavigate()
+  const toDetailLinkPageEventlistener = () => {
+    console.log('event detail listener run');
+    navigate('./detail');
+  };
+
+  const handleKeyDownEnter = (event) => {
+    if (event.code === 'enter') {
+      toDetailLinkPageEventlistener();
+    }
+  };
+
   const {
     vehicleLogoSrc, carModelName, numberOfModelsAvailable, colour,
   } = props;
   return (
     <>
-      <div className={colour ? 'estimateDiv differentColour' : 'estimateDiv'}>
-        <div className="imageLogoContainer">
-          <img alt="vehicle logo" className="logoImage" src={vehicleLogoSrc} />
+      <button className="button-container" type="button" onClick={toDetailLinkPageEventlistener} onKeyDown={handleKeyDownEnter}>
+        <div className={colour ? 'estimateDiv differentColour' : 'estimateDiv'}>
+          <div className="imageLogoContainer">
+            <img alt="vehicle logo" className="logoImage" src={vehicleLogoSrc} />
+          </div>
+          <p className="carModelDescriptionText">
+            {carModelName}
+          </p>
+          <p className="carModelDescriptionText sizeOfNumberText">
+            {numberOfModelsAvailable}
+          </p>
         </div>
-        <p className="carModelDescriptionText">
-          {carModelName}
-        </p>
-        <p className="carModelDescriptionText sizeOfNumberText">
-          {numberOfModelsAvailable}
-        </p>
-      </div>
+      </button>
     </>
   );
 };
