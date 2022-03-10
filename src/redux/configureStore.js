c467cb0c1ae3680d063c81926650b2767352dfe8
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-10 18:17:10 +0200

    Add redux-logger and pass it as redux middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import carModelsReducer from './reduxSlices/carModelSlice';
 
 const store = configureStore({
   reducer: {
     carModels: carModelsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
