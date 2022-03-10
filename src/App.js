82e516052b1274e2dd9ecd366631226baa1e7784
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-10 17:29:18 +0200

    Add Routes in main app.js page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import DeatailsPage from './pages/details/details';
 import Home from './pages/home/home';
 
 function App() {
   return (
-    <Home />
+    <>
+      <h1 className="mainHeaderText"> VEHICLE CARBON ESTIMATE STATS PER 100KM</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<DeatailsPage />} />
+      </Routes>
+    </>
+
   );
 }
 
