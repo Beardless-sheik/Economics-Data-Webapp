270549d348fbdcc9d85ac3b5f0518193bd9ffd6e
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-10 16:38:33 +0200

    Display home page categories of cars and add pagination to the moultiple results obtained from the API

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,57 +1,111 @@
-import { Fragment, useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import './home.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import ReactPaginate from 'react-paginate';
 import ModelCarDetails from '../../components/ModelCarDetails/modelCarDetails';
 import { fetchCarModels } from '../../redux/reduxSlices/carModelSlice';
 
+const CarModelList = ({ carModelList }) => {
+  let keyData = 1;
+  console.log(carModelList);
+  return (
+    <>
+      {carModelList
+        && carModelList.map((item, index) => {
+          keyData += 1;
+          if (index % 1.5 === 0) {
+            return (
+              <ModelCarDetails
+                colour="different"
+                key={keyData}
+                vehicleLogoSrc="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg"
+                carModelName={item.data.attributes.name}
+                numberOfModelsAvailable={item.data.attributes.number_of_models}
+              />
+            );
+          }
+          return (
+            <ModelCarDetails
+              key={keyData}
+              vehicleLogoSrc="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg"
+              carModelName={item.data.attributes.name}
+              numberOfModelsAvailable={item.data.attributes.number_of_models}
+            />
+          );
+        })}
+    </>
+  );
+};
+
+function PaginatedItems({ itemsPerPage }) {
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const carModels = useSelector((state) => state.carModels);
+  console.log(carModels);
+
+  useEffect(() => {
+    console.log('pagnation useEffect');
+    // let {carModelsDestruct} = carModels.carModels;
+    console.log(carModels.carModels);
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    if (carModels.carModels.length !== 0) {
+      console.log(currentItems);
+      setCurrentItems(carModels.carModels.slice(itemOffset, endOffset));
+      console.log(currentItems);
+      setPageCount(Math.ceil(carModels.carModels.length / itemsPerPage));
+    }
+  }, [itemOffset, itemsPerPage, carModels]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % carModels.carModels.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`,
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <CarModelList carModelList={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="-->"
+        previousLabel="<--"
+        onPageChange={handlePageClick}
+        pageCount={pageCount}
+        renderOnZeroPageCount={null}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </>
+  );
+}
+
 const Home = () => {
-  // const carModels = useSelector((state) => state);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCarModels());
   }, []);
-
-  let keyData = 1;
-  const ModelArrayData = [{
-    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
-    carModelName: 'Alfa Romeo',
-    numberOfModelsAvailable: 567,
-  }, {
-    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
-    carModelName: 'BMW',
-    numberOfModelsAvailable: 1567,
-  }, {
-    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
-    carModelName: 'Benz',
-    numberOfModelsAvailable: 5567,
-  }, {
-    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
-    carModelName: 'Lambo',
-    numberOfModelsAvailable: 55667,
-  },
-  {
-    vehicleLogoSrc: 'https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg',
-    carModelName: 'Nissan',
-    numberOfModelsAvailable: 6856,
-  }];
   return (
     <>
       <h1 className="mainHeaderText"> VEHICLE CARBON ESTIMATE STATS PER 100KM</h1>
       <p className="subMainHeaderText">STATS By Models</p>
       <div className="mainEstimateGrid">
-        {
-            ModelArrayData.map((element) => {
-              keyData += 1;
-              return (
-                <ModelCarDetails
-                  key={keyData}
-                  vehicleLogoSrc={element.vehicleLogoSrc}
-                  carModelName={element.carModelName}
-                  numberOfModelsAvailable={element.numberOfModelsAvailable}
-                />
-              );
-            })
-        }
+        <PaginatedItems itemsPerPage={10} />
       </div>
     </>
   );
