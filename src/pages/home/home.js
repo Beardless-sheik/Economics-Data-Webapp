6f32b128fa853d14dd91eca698708b2b237fecd8
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-11 13:12:49 +0200

    Pass Id of selected car to the Car Details
    Will be used for further API processing

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,18 +6,16 @@ import PropTypes from 'prop-types';
 import ModelCarDetails from '../../components/ModelCarDetails/modelCarDetails';
 import { fetchCarModels } from '../../redux/reduxSlices/carModelSlice';
 
-const CarModelList = ({ carModelList }) => {
-  let keyData = 1;
-  return (
-    <>
-      {carModelList
+const CarModelList = ({ carModelList }) => (
+  <>
+    {carModelList
         && carModelList.map((item, index) => {
-          keyData += 1;
           if (index % 1.5 === 0) {
             return (
               <ModelCarDetails
                 colour="different"
-                key={keyData}
+                key={item.data.id}
+                modelId={item.data.id}
                 vehicleLogoSrc="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg"
                 carModelName={item.data.attributes.name}
                 numberOfModelsAvailable={item.data.attributes.number_of_models}
@@ -26,16 +24,16 @@ const CarModelList = ({ carModelList }) => {
           }
           return (
             <ModelCarDetails
-              key={keyData}
+              key={item.data.id}
+              modelId={item.data.id}
               vehicleLogoSrc="https://c8.alamy.com/comp/D12RG7/logo-of-the-make-alfa-romeo-of-the-italian-car-manufacturer-fiat-group-D12RG7.jpg"
               carModelName={item.data.attributes.name}
               numberOfModelsAvailable={item.data.attributes.number_of_models}
             />
           );
         })}
-    </>
-  );
-};
+  </>
+);
 
 CarModelList.propTypes = {
   carModelList: PropTypes.arrayOf(PropTypes.shape).isRequired,
