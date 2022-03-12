2a317c0044e0ba79eea3d93cf3e39fd468722f1f
Alick Nyirenda <nyirenda_a@zoho.com>
2022-03-12 15:46:06 +0200

    Add extra route for estimate page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import DeatailsPage from './pages/details/details';
+import EstimateDetailsPage from './pages/estimateDetails/estimateDetails';
 import Home from './pages/home/home';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <h1 className="mainHeaderText"> VEHICLE CARBON ESTIMATE STATS PER 100KM</h1>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="detail" element={<DeatailsPage />} />
+        <Route path="/detail" element={<DeatailsPage />} />
+        <Route path="/detail/estimate" element={<EstimateDetailsPage />} />
       </Routes>
     </>
 
